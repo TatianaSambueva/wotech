5c2ca424dc1bf80fd0903dee4c2e4d6bd198c006
TatianaSambueva <124908819+TatianaSambueva@users.noreply.github.com>
2024-04-03 19:21:27 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,42 @@
+public class Main {
+  public static void main(String[] args) {
+
+    // winter, spring, summer, autumn
+    // warm jacket, T-shirt, swimming suite, rain coat
+
+    String season = "spring";
+
+    if (season == "winter") {
+      System.out.println("Wear a warm jacket!");
+    }
+    else if (season == "spring") {
+      System.out.println("Wear a T-shirt!");
+    }
+    else if (season == "summer") {
+      System.out.println("Wear a swimming suite!");
+    }
+    else (season == "autumn") {
+      System.out.println("Wear a rain coat!");
+    }
+
+    double temperature = +5;
+    
+    if (temperature <= 5) {
+      System.out.println("Wear super warm!");
+    }
+    else if (temperature <=  15) {
+      System.out.println("Wear warm!");
+    }
+    else if (temperature <= +30) {
+      System.out.println("Normal");
+    }
+    else (temperature >= +30) {
+      System.out.println("Hot");
+    }
+    
+  
+  
+  
+  }
+
+}
